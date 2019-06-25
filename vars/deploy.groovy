da8cc665b952b1fd784437b97292e84fd066b6be
James Eby <temporafugiunt@gmail.com>
2019-06-25 11:58:52 -0500

    Adding namespace into deployment name for app deployments.

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -32,7 +32,7 @@ void call(BuildInfo buildInfo, String repository, String imageName)
                             helm init --client-only
                             helm plugin install https://github.com/chartmuseum/helm-push
                             helm repo add --username ${REGISTRY_USER} --password ${REGISTRY_USER_PASSWORD} ${REPOSITORY} https://${REGISTRY_URL}/chartrepo/${REPOSITORY}
-                            helm upgrade --install --namespace ${NAMESPACE} ${IMAGE_NAME} $REPOSITORY/${IMAGE_NAME} --version ${VERSION} --set image.tag=${APPVERSION}
+                            helm upgrade --install --namespace ${NAMESPACE} ${NAMESPACE}-${IMAGE_NAME} $REPOSITORY/${IMAGE_NAME} --version ${VERSION} --set image.tag=${APPVERSION}
                             '''
                         }
                     }
