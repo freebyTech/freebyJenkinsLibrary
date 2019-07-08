c6e537bcb5097c067c8dffedd458fbdcfe823eb9
James Eby <temporafugiunt@gmail.com>
2019-07-08 11:43:50 -0500

    namespace is acting strange, changing variable name.

--- vars/deployVersion.groovy
+++ vars/deployVersion.groovy
@@ -1,7 +1,7 @@
 import com.freebyTech.BuildInfo
 import com.freebyTech.BuildConstants
 
-void call(def script, String version, String repository, String imageName, String namespace) 
+void call(def script, String version, String repository, String imageName, String ns) 
 {
     BuildInfo buildInfo = new BuildInfo(steps, script)
 
@@ -26,7 +26,7 @@ void call(def script, String version, String repository, String imageName, Strin
             {      
                 container('freeby-agent') 
                 {
-                    withEnv(["APPVERSION=${buildInfo.version}", "VERSION=${buildInfo.semanticVersion}", "REPOSITORY=${repository}", "IMAGE_NAME=${imageName}", "NAMESPACE=${namespace}"])
+                    withEnv(["APPVERSION=${buildInfo.version}", "VERSION=${buildInfo.semanticVersion}", "REPOSITORY=${repository}", "IMAGE_NAME=${imageName}", "NAMESPACE=${ns}"])
                     {
                         // Need registry credentials for agent build operation to setup chart museum connection.
                         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: env.REGISTRY_USER_ID,
