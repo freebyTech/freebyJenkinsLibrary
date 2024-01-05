551f4c5e5c85a0cb6d6ad3ac2179e2c65474786b
James Eby <admin@freebytech.com>
2024-01-05 01:50:19 -0600

    Trying a different way to get origin URL.

--- src/com/freebyTech/BuildInfo.groovy
+++ src/com/freebyTech/BuildInfo.groovy
@@ -8,7 +8,6 @@ class BuildInfo implements Serializable {
     String tag
     String agentTag
     String registry
-    String originUrl
 
     private def steps
     private def script
@@ -71,7 +70,6 @@ class BuildInfo implements Serializable {
 
     def checkForVersionOverrideTags(String versionPrefix) {
         def lastVersion = script.sh(returnStdout: true, script: "echo \$(git tag --sort=-creatordate -l 'v${versionPrefix}.*' | head -1)").trim()
-        this.originUrl = script.sh(returnStdout: true, script: 'git config --get remote.origin.url').trim()
         if (lastVersion.length() > 0) {
             steps.echo "Existing version tag ${lastVersion} found"
             lastVersion = lastVersion.substring(1);
@@ -90,7 +88,8 @@ class BuildInfo implements Serializable {
     def pushTag() {
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: script.env.PRIVATE_GIT_REPO_USER_ID, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
             steps.echo "Pushing new version tag ${this.version}"
-            def fixedOriginUrl = this.originUrl.replaceAll("https://", "https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}")
+            def originUrl = script.scm.getUserRemoteConfigs()[0].getUrl()
+            def fixedOriginUrl = originUrl.replaceAll("https://", "https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}")
             script.sh(script:"git config --global --add safe.directory ${script.pwd()}")
             script.sh(script:"git config --global user.email \"${script.env.GIT_USER_EMAIL}\"")
             script.sh(script:"git config --global user.name \"${script.env.GIT_USER_NAME}\"")
